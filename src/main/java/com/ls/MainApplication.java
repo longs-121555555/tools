2651705a98a52f17c5fa473131bc2aeeab2ab076
longsheng <34882748+longs12155555@users.noreply.github.com>
2021-07-28 14:45:27 +0800

    Update MainApplication.java

--- src/main/java/com/ls/MainApplication.java
+++ src/main/java/com/ls/MainApplication.java
@@ -1,6 +1,5 @@
 package com.ls;
 
-import com.ls.tools.base64.Base64Util;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,10 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class MainApplication {
 
     public static void main(String[] args) {
-        //SpringApplication.run(MainApplication.class, args);
-        String decoder = Base64Util.decoder("{\"name\":\"admin\",\"pwd\":123456}");
-        System.out.println(decoder);
-        String encode = Base64Util.encode(decoder);
-        System.out.println(encode);
+        SpringApplication.run(MainApplication.class, args);
     }
 }
